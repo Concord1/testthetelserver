58d4637d19ce9b57c181ce815fded798371dced9
unknown <harishkanagal@gmail.com>
2023-06-28 21:10:31 +0530

    FIRST COMMIT

---
+++ index.js
@@ -0,0 +1,50 @@
+// Import dependencies
+const express = require('express');
+const dotenv = require('dotenv');
+const path = require('path');
+const fetch = require('node-fetch');
+
+// Load environment variables
+dotenv.config();
+const serverapiURL = process.env.SEND_TO_DYNAMODB_API;
+
+// Create Express app
+const app = express();
+
+// Set the port
+const port = process.env.PORT || 3300;
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Define a route for the homepage
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+app.post('/sendtoDynamo', async (request, response) =>{
+    console.log("HIIII")
+    try {
+        const fetchResponse = await fetch(serverapiURL, request);
+        const data = await fetchResponse.json();
+        console.log(data);
+        response.json({ message: 'Fetch request completed successfully' });
+      } catch (error) {
+        console.error('Error:', error);
+        response.status(500).json({ error: 'Internal server error' });
+      }
+})
+
+
+app.get('/dinoname', async (request, response) => {
+    const dinoNameResponse = {'sample':'json'};
+    const usingApiKey = {serverapiURL};
+    console.log(usingApiKey);
+	response.json(dinoNameResponse);
+});
