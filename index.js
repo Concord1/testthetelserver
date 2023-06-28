dec42474961370d595dc18ddd0e9bd5fe98d40fc
Harish Kanagal <78932555+Concord1@users.noreply.github.com>
2023-06-28 23:05:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.listen(port, () => {
 });
 
 app.post('/sendtoDynamo', async (request, response) =>{
-    console.log(request)
+    console.log(request.body)
     try {
 	delete request.headers.host;
   	delete request.headers.referer;
