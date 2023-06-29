dc3153bb2b6b103af7fdc1d7d604450b19ce23d1
Harish Kanagal <78932555+Concord1@users.noreply.github.com>
2023-06-29 11:22:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,8 +36,8 @@ app.post('/sendtoDynamo', async (request, response) =>{
   	delete request.headers.referer;
 	console.log(request.body)
         const fetchResponse = await fetch(serverapiURL, request);
-	//console.log(fetchResponse);
-	console.log(fetchResponse.headers.get('content-type'));
+	// //console.log(fetchResponse);
+	// console.log(fetchResponse.headers.get('content-type'));
       } catch (error) {
         console.error('Error:', error);
         response.status(500).json({ error: 'Internal server error' });
